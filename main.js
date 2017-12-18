1d7400c92c7464cff4ed6afc0e7863af85609912
DaseulKim <da91love@naver.com>
2017-12-18 18:27:46 +0900

    팝업창 만들면 끝

--- main.js
+++ main.js
@@ -5,40 +5,47 @@ app.use(bodyParser.urlencoded({extended : false}));
 var fs = require('fs');
 
 var request = require('request');
+var rp = require('request-promise');
 var cheerio = require('cheerio');
+//var player = require('play-sound')({player: "C:/Program Files/Daum/PotPlayer/PotPlayer64.exe"});
 var player = require('play-sound')({player: "C:/Program Files/Daum/PotPlayer/PotPlayer64.exe"});
 
+function coinPrice(coinName){
+	var priceArr=[];
+	setInterval(function(){
+		var encoding1 =encodeURIComponent('相場チャート');
+		var encoding2 =encodeURIComponent(coinName);
+		var reqParam = 'https://www.coingecko.com/ja/'+encoding1+'/'+encoding2+'/jpy';
 
-function findPrice(){
-	var player = require('play-sound')({player: "C:/Program Files/Daum/PotPlayer/PotPlayer64.exe"});
-	var pagdAddr = 'https://www.coingecko.com/ja/%E7%9B%B8%E5%A0%B4%E3%83%81%E3%83%A3%E3%83%BC%E3%83%88/%E3%83%AA%E3%83%83%E3%83%97%E3%83%AB/jpy';
-	request(pagdAddr, function (error, response, body) {
-		if(error){
-			console.log('에러발생');
-		}
-		else{
+		request(reqParam, function (error, response, body) {
 			var $ = cheerio.load(body);
-			var price = $('.coin-value').children('span').text();
-			var percent = $('.stat-percent-lg').text();	
-			
-			price = price.trim().substring(0,price.trim().length-1);
-			percent = percent.trim().substring(0,percent.trim().length-1);
-			console.log(price.trim());
-			console.log(percent.trim());
-			
-			if(parseInt(percent)>30){
-				player.play('./sound/alarm.mp3', function(err){
-					  if (err) throw err
-				});
-				setTimeout(function(){
-					player.pause();}
-					, 3000);
+
+			var price = $('.mt-2').find('td').eq(2).children('span').text().trim();
+			console.log('price : ' + price);
+			console.log('timestrap :' + Math.floor(Date.now() / 1000));
+			price = price.substring(0,price.length-1);
+			price = price.replace(/[,]/g,"");
+			price = parseFloat(price);
+
+			if(priceArr[0]===undefined){
+//				console.log('if priceArr[0]: ' + priceArr[0]);
+//				console.log('if price: ' + priceArr[0]);
+				priceArr[0] = price;
 			}
-			
-		}
-	});	
+			else{
+//				console.log('else priceArr[0]: ' + priceArr[0]);
+//				console.log('else price: ' + priceArr[0]);
+				var variation = ((price-priceArr[0])/priceArr[0])*100;
+				console.log(variation + '%');
+				if (((price-priceArr[0])/priceArr[0])*100 > 2){
+					console.log('attention');
+				}
+				priceArr[0] = price;
+
+			}
+		})
+
+		},60000);
 }
 
-setInterval(function(){
-	findPrice();
-	}, 3000);
+coinPrice('モナーコイン');
