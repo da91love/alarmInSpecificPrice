2186052f510cb706928807fd5ece8fa396c00fe7
DASEUL KIM <da91love@naver.com>
2017-12-19 09:09:34 +0900

    end

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended : false}));
-var fs = require('fs');
+var dialog = require('dialog');
 
 var request = require('request');
 var rp = require('request-promise');
@@ -11,6 +7,7 @@ var cheerio = require('cheerio');
 var player = require('play-sound')({player: "C:/Program Files/Daum/PotPlayer/PotPlayer64.exe"});
 
 function coinPrice(coinName){
+	var d = new Date();
 	var priceArr=[];
 	setInterval(function(){
 		var encoding1 =encodeURIComponent('相場チャート');
@@ -18,11 +15,11 @@ function coinPrice(coinName){
 		var reqParam = 'https://www.coingecko.com/ja/'+encoding1+'/'+encoding2+'/jpy';
 
 		request(reqParam, function (error, response, body) {
-			var $ = cheerio.load(body);
-
+			var $ = cheerio.load(body);	
 			var price = $('.mt-2').find('td').eq(2).children('span').text().trim();
-			console.log('price : ' + price);
-			console.log('timestrap :' + Math.floor(Date.now() / 1000));
+			console.log(coinName+'price : ' + price);
+//			console.log('timestrap :' + d.getFullYear() + "/"+d.getMonth()+"/"+ d.getDate()+" "
+//					+ d.getHours() +':'+ d.getMinutes() +':'+ d.getSeconds());
 			price = price.substring(0,price.length-1);
 			price = price.replace(/[,]/g,"");
 			price = parseFloat(price);
@@ -36,16 +33,27 @@ function coinPrice(coinName){
 //				console.log('else priceArr[0]: ' + priceArr[0]);
 //				console.log('else price: ' + priceArr[0]);
 				var variation = ((price-priceArr[0])/priceArr[0])*100;
-				console.log(variation + '%');
-				if (((price-priceArr[0])/priceArr[0])*100 > 2){
-					console.log('attention');
+				console.log(coinName+'variation:'+variation + '%');
+				if (((price-priceArr[0])/priceArr[0])*100 > 1){
+					var soundPath ='./sound/Alarm.mp3';
+					player.play(soundPath, function(err){
+						  if (err) throw err
+						});
+					//dialog.info(coinName + '1%이상 가격상승');
+				}
+				
+				else if(((price-priceArr[0])/priceArr[0])*100 < -1){
+					var soundPath ='./sound/Alarm.mp3';
+					player.play(soundPath, function(err){
+						  if (err) throw err
+						});
+					//dialog.info(coinName + '1%이하 가격하락');
 				}
 				priceArr[0] = price;
-
 			}
 		})
-
-		},60000);
+		},30000);
 }
 
 coinPrice('モナーコイン');
+coinPrice('リップル');
